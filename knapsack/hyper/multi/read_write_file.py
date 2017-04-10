b5bdb9956a54de77ac9b2829c3d1848fbfaa414e
gcinbax <gcinbax@gmail.com>
2017-04-10 21:07:38 +0300

    read test data (mknapcb*)

--- knapsack/hyper/multi/read_write_file.py
+++ knapsack/hyper/multi/read_write_file.py
@@ -1,7 +1,6 @@
 ################################################################################################
 #               http://people.brunel.ac.uk/~mastjjb/jeb/orlib/mknapinfo.html                   #
 ################################################################################################
-from pprint import pprint
 
 
 def parse_mknap1(path_to_file):
@@ -63,6 +62,39 @@ def parse_mknap2(path_to_file):
     return results
 
 
+def parse_mknapcb(path_to_file):
+    results = []
+    with open(path_to_file) as file:
+        number_of_cases = int(file.readline())
+        for case in range(number_of_cases):
+            _, item_number, ksp_number, _, _ = file.readline().split(' ')
+            item_number = int(item_number)
+            ksp_number = int(ksp_number)
+
+            costs = []
+            while len(costs) < item_number:
+                costs += [float(cost) for cost in file.readline().split(' ') if cost not in ('', '\n')]
+
+            weights = []
+            for i in range(ksp_number):
+                current_weight = []
+                while len(current_weight) < item_number:
+                    current_weight += [int(weight) for weight in file.readline().split(' ') if weight not in ('', '\n')]
+                weights.append(current_weight)
+
+            sizes = []
+            while len(sizes) < ksp_number:
+                sizes += [float(size) for size in file.readline().split(' ') if size not in ('', '\n')]
+
+            results.append({
+                'costs': costs,
+                'weights': weights,
+                'sizes': sizes
+            })
+    return results
+
+
 if __name__ == '__main__':
     # pprint(parse_mknap1("./resources/mknap1.txt"))
-    pprint(parse_mknap2("./resources/mknap2.txt"))
+    # pprint(parse_mknap2("./resources/mknap2.txt"))
+    parse_mknapcb("./resources/mknapcb1.txt")
