bd541400c0f5a2cb47db0ee60c5401a6864e5312
gcinbax <gcinbax@gmail.com>
2017-05-15 18:09:20 +0300

    remove redundant heuristics

--- knapsack/hyper/multidim/heuristics.py
+++ knapsack/hyper/multidim/heuristics.py
@@ -10,15 +10,10 @@ def get_heuristics():
     if cached_heuristics is not None:
         return cached_heuristics
     # probability to select add heuristic is 0.9
-    add_heurs = [add_lightest, add_heaviest, add_least_cost, add_most_cost, add_best,
-                 add_heaviest_item_heaviest_condition, add_lightest_item_heaviest_condition,
-                 add_heaviest_item_lightest_condition,
-                 add_lightest_item_lightest_condition]
+    add_heurs = [add_lightest, add_heaviest, add_least_cost, add_most_cost, add_best]
     add_heurs = list(zip(add_heurs, [0.8 / len(add_heurs)] * len(add_heurs)))
     # probability to select remove heuristic is 0.1
-    remove_heurs = [remove_lightest, remove_heaviest, remove_least_cost, remove_most_cost, remove_worst,
-                    remove_heaviest_item_heaviest_condition, remove_lightest_item_heaviest_condition,
-                    remove_heaviest_item_lightest_condition, remove_lightest_item_lightest_condition]
+    remove_heurs = [remove_lightest, remove_heaviest, remove_least_cost, remove_most_cost, remove_worst]
     remove_heurs = list(zip(remove_heurs, [0.2 / len(remove_heurs)] * len(remove_heurs)))
     cached_heuristics = add_heurs + remove_heurs
     return cached_heuristics
